2b9e542ebb36384bb92e9f6d10b323d2dc790fae
Caterinkin <caterinkin@gmail.com>
2025-02-09 14:43:38 +0300

    Update main.cpp
    переписала практически полностью

--- main.cpp
+++ main.cpp
@@ -1,95 +1,69 @@
-﻿#include <iostream>
+#include <iostream>
 #include <windows.h>
 #include <string>
 
-
-class Counter
+class Counter 
 {
-private:
-	char symbol;
-	int num = 1;
-public:
-	Counter()
-	{
-	}
-	Counter(char symbol)
-	{
-		this->symbol = symbol;
-	}
-	Counter(int num)
-	{
-		this->num = num;
-	}
-	void enter_num(int num)
-	{
-		this->num = num;
-	}
-	void progres(char symbol)
-	{
-		if (symbol == '+')
-		{
-			++num;
-		}
-		if (symbol == '-') 
-		{ 
-			--num;
-		}
-		if (symbol == '=')
-		{
-			std::cout << num << std::endl;
-		}
-	}
-
-};
-
+   private:
+       int value;
+   public:
+       Counter() : value(1) {}
+       Counter(int initialValue) : value(initialValue) {}
+       void increment() 
+       {
+           ++value;
+       }
+       void decrement() 
+       {
+           --value;
+       }
+       int getValue() const 
+       {
+           return value;
+       }
+   };
 int main()
 {
-	setlocale(LC_ALL, "rus");
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+   
+    int initialValue = 1;
+    std::string choice;
+   
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> choice;
+    
+	if (choice == "да" || choice == "Да") 
+    {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> initialValue;
+    }
+    Counter counter(initialValue);
+    char command;
+    while (true)
+   {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> command;
 
-	
-	Counter count;
-	std::string answer;
-	char symbol;
-	int i = 0, start;
-	std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
-	std::cin >> answer;
-	do
+        switch (command) 
 	{
-		if (answer == "да")
-		{
-			std::cout << std::endl << "Введите начальное значение счётчика: ";
-			std::cin >> start;
-			i = 1;
-			count.enter_num(start);
-	    }
-		else 
-		{
-			std::cout << "Вы отказались от запуска счетчика! " << std::endl;
-			return 0;
-	    }
-	} while (i==0);
-
-	do
-	{
-		std::cout << "Введите команду ('+', '-', '=' или 'x'):";
-		std::cin >> symbol;
-		std::cout << std::endl;
-		if (symbol == '+')
-		{
-			count.progres(symbol); continue;
-		}
-		if (symbol == '-')
-		{
-			count.progres(symbol); continue;
-		}
-		if (symbol == '=')
-		{
-			count.progres(symbol); continue;
-		}
-	} while (symbol != 'x');
-	std::cout << "До свидания!" << std::endl;
-
-	return EXIT_SUCCESS;
+        case '+':
+            counter.increment();
+            break;
+        case '-':
+            counter.decrement();
+            break;
+        case '=':
+            std::cout << "Текущее значение счётчика: " << counter.getValue() << std::endl;
+            break;
+        case 'x':
+            std::cout << "Завершение работы программы." << std::endl;
+            return 0;
+        default:
+            std::cout << "Неизвестная команда. Попробуйте снова." << std::endl;
+            break;
+        }
+    }
+    return EXIT_SUCCESS;;
 }
